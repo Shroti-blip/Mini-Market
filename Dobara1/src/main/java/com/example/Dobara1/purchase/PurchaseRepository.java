5f3f8a006e0a644daf873b0f692c2d4379f8556c
Muskan Shroti <shrotimuskan@gmail.com>
2025-10-14 22:41:06 +0530

    Update PurchaseRepository.java

--- Dobara1/src/main/java/com/example/Dobara1/purchase/PurchaseRepository.java
+++ Dobara1/src/main/java/com/example/Dobara1/purchase/PurchaseRepository.java
@@ -1,6 +1,11 @@
 package com.example.Dobara1.purchase;
 
+import com.example.Dobara1.listingitem.ListingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PurchaseRepository extends JpaRepository<PurchaseEntity  , Integer> {
+    List<PurchaseEntity> findBySeller_UserId(Integer sellerId);
+
 }
